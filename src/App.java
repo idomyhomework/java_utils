01d6c6cf8588af68b26bb8f57dc852eacc6b0414
idomyhomework <alex.scheglov25@gmail.com>
2026-02-12 20:12:26 +0100

    added quicksort sort overload

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
 
-        int[] arr = { 45, 34, 55, 66, 67, 90, 1, -34, 0, 123 };
+        int[] arr = { 45, 34, 55, 66, 67, 90, 1, -34, 0, 123, 78, -1, -30, -1024, 569 };
 
         Quicksort.sort(arr, 0, arr.length - 1);
 
