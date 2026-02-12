ff0664384c77fff0330d9b95e2a0f329bbe3fa3f
idomyhomework <alex.scheglov25@gmail.com>
2026-02-12 19:59:16 +0100

    Some not important lines remove.

--- src/Burbuja.java
+++ src/Burbuja.java
@@ -1,60 +1,8 @@
 import java.util.Random;
 
 public class Burbuja {
-    private int[] datos;
-    private String[] datosTexto;
 
-    public Burbuja(int n, int min, int max) {
-        datos = new int[n];
-        generarDatos(n, Math.min(min, max), Math.max(min, max));
-    }
-
-    private void generarDatos(int n, int min, int max) {
-        Random rand = new Random();
-
-        for (int i = 0; i < datos.length; i++) {
-            datos[i] = rand.nextInt(min, max);
-        }
-    }
-
-    public Burbuja(int len, int lenPalabra) {
-        datosTexto = new String[len];
-        for (int i = 0; i < datosTexto.length; i++) {
-            datosTexto[i] = generarPalabra(97, 122, lenPalabra);
-        }
-    }
-
-    public String toString() {
-        final int ANCHO = 5;
-        String salida = "";
-        for (int i = 0; i < datos.length; i++) {
-            if (i != datos.length - 1)
-                salida += datos[i] + ", ";
-            else
-                salida += datos[i];
-            if (i != 0 && i % ANCHO == 0)
-                salida += "\n";
-        }
-
-        return salida;
-    }
-
-    public String toStringWords() {
-        final int ANCHO = 50;
-        String salida = "";
-        for (int i = 0; i < datosTexto.length; i++) {
-            if (i != datosTexto.length - 1)
-                salida += datosTexto[i] + ", ";
-            else
-                salida += datosTexto[i];
-            if (i != 0 && i % ANCHO == 0)
-                salida += "\n";
-        }
-
-        return salida;
-    }
-
-    public void ordenar() {
+    public void sort(int[] datos) {
         for (int i = 1; i < datos.length - 1; i++) {
             for (int j = 0; j < datos.length - i; j++) {
                 if (datos[j] > datos[j + 1]) {
@@ -66,7 +14,7 @@ public class Burbuja {
         }
     }
 
-    public void ordenarMejorado() {
+    public void improvedSort(int[] datos) {
         int i = 0;
         boolean esOrdenado = false;
 
@@ -85,50 +33,7 @@ public class Burbuja {
 
     }
 
-    public String generarPalabra(int min, int max, int len) {
-        Random rand = new Random();
-        String resultado = "";
-        for (int i = 0; i < len; i++) {
-            int numRand = rand.nextInt(min, max);
-            char ch = (char) numRand;
-            resultado += String.valueOf(ch);
-        }
-        return resultado;
-    }
-
-    public void ordenarPalabras() {
-        int i = 0;
-
-        while (i < datosTexto.length) {
-            i++;
-            for (int j = 0; j < datosTexto.length - i; j++) {
-                if (datosTexto[j].charAt(0) > datosTexto[j + 1].charAt(0)) {
-                    String aux = datosTexto[j];
-                    datosTexto[j] = datosTexto[j + 1];
-                    datosTexto[j + 1] = aux;
-                }
-            }
-        }
-
-        for (int a = 0; a < datosTexto.length - 1; a++) {
-            if (datosTexto[a].charAt(0) == datosTexto[a + 1].charAt(0)) {
-                int o = 1;
-                boolean esOrdenadoMejor = false;
-                while (!esOrdenadoMejor) {
-                    if (datosTexto[a].charAt(o) > datosTexto[a + 1].charAt(o)) {
-                        String aux = datosTexto[a];
-                        datosTexto[a] = datosTexto[a + 1];
-                        datosTexto[a + 1] = aux;
-                        esOrdenadoMejor = true;
-                    } else {
-                        o++;
-                    }
-                }
-            }
-        }
-    }
-
-    public static void bidireccional(int arr[]) {
+    public static void sortBidireccional(int arr[]) {
         int izq = 0;
         int der = arr.length - 1;
         boolean hayCambios = true;
